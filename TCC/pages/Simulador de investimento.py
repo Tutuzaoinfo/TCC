07ae74a34142188332850576b96c751b7166c214
Tutuzao <arthurdoidodms@gmail.com>
2025-11-09 22:48:43 -0300

    Update Simulador de investimento.py

--- TCC/pages/Simulador de investimento.py
+++ TCC/pages/Simulador de investimento.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
 
 st.set_page_config(page_title="Calculadora & Simulador — Renda Fixa", layout="wide")
 
